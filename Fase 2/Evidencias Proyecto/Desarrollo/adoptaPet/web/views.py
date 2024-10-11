3acbed885fc2e31ed86cb881a59a78c020fde964
“XimenaValderrama” <xi.valderrama@duocuc.cl>
2024-10-11 02:53:13 -0300

    cambios app web

--- Fase 2/Evidencias Proyecto/Desarrollo/adoptaPet/web/views.py
+++ Fase 2/Evidencias Proyecto/Desarrollo/adoptaPet/web/views.py
@@ -17,3 +17,24 @@ def perfil(request):
 def tenencia_responsable(request):
 
     return render(request, "tenencia_responsable.html")
+
+def adopcion(request):
+
+    return render(request, "adopcion.html")
+
+def adoptados(request):
+
+    return render(request, "adoptados.html")
+
+def busqueda(request):
+
+    return render(request, "busqueda.html")
+
+def encontrados(request):
+
+    return render(request, "encontrados.html")
+
+
+def registro(request):
+
+    return render(request, "registro.html")
